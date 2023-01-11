d3cab88990d67c005450955926f07153cf751a4e
Surajk37 <surajkhot9993@gmail.com>
2023-01-11 16:48:57 +0530

    [Suraj] To enter a valid First Name

--- UserRegistrationProblemRegex/Program.cs
+++ UserRegistrationProblemRegex/Program.cs
@@ -5,6 +5,22 @@
         static void Main(string[] args)
         {
             Console.WriteLine("\t\tWelcome To User Registration Problem using Regex\n");
+            Console.WriteLine("Please Select option from below List");
+            Console.WriteLine("\n1. Validate First Name ");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:      
+                    Console.WriteLine("\t  Enter First Name");
+                    Console.WriteLine("\t--------------------");
+                    string firstName = Console.ReadLine();
+                    bool firstNameResult = UserRegistration.ValidateFirstName(firstName);
+                    UserRegistration.PrintResult(firstNameResult);
+                    break;           
+                default:
+                    Console.WriteLine("Please Enter Valid Number");
+                    break;
+            }
             Console.ReadLine();
         }
     }
---
+++ UserRegistrationProblemRegex/UserRegistration.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistrationProblemRegex
+{
+    class UserRegistration
+    {
+        public static string REGEX_FIRSTNAME = "^[A-Z][a-z]{2,}$";   //regex pattern to get minimum 3 character at least 1 capital letter
+
+           //Method to test first name
+        public static bool ValidateFirstName(string firstName)
+        {
+           return Regex.IsMatch(firstName, REGEX_FIRSTNAME);
+        }
+      
+        //To print the result
+        public static void PrintResult(bool result)
+        {
+           if (result)
+           {
+             Console.WriteLine("Valid.");
+           }
+           else
+           {
+             Console.WriteLine("Invalid.");
+           }
+        }
+    }
+}
